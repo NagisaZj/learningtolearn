f399b039413eee3d7e2b3ffe6a59094ac1145912
jin zhang <1170863106@qq.com>
2018-04-19 18:03:25 +0800

    okqwe

--- mnist_case.py
+++ mnist_case.py
@@ -12,7 +12,7 @@ layers = 2
 #batch_size = 1
 lr = 1e-3
 full_batch = 128
-train_steps = 10
+train_steps = 1000
 sess = tf.Session()
 
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
@@ -33,23 +33,29 @@ class train:
         self.out_grads()
         self.build_opti()
         self.apply_grads()
-        #self.update()
-        tl.layers.initialize_global_variables(sess)
+        self.update()
+        #tl.layers.initialize_global_variables(sess)
+        self.sess.run(tf.global_variables_initializer())
 
     def build_target_net(self,times):
         with tf.variable_scope("optimizee%d"%times) as scope:
             w_init =tf.truncated_normal_initializer(stddev=0.1)
             self.input = tf.placeholder(tf.float32,[None,n_dimension])
-            net = tl.layers.InputLayer(self.input,"In")
-            net = tl.layers.DenseLayer(net,n_units=net_size,act = tf.nn.sigmoid,W_init = w_init, name="sigmoid1")
-            net = tl.layers.DenseLayer(net,n_units=10,act =tf.nn.softmax,W_init = w_init, name="softmax1")
-            self.output = net.outputs
+            self.W1 = tf.Variable(tf.random_uniform([n_dimension, net_size], -1.0, 1.0), name = 'W1')
+            self.b1 = tf.Variable(tf.zeros(shape=[net_size]), name='b1')
+            self.y1 = tf.nn.sigmoid(tf.matmul(self.input, self.W1) + self.b1)
+            self.W2 = tf.Variable(tf.random_uniform([net_size, 10], -1.0, 1.0), name='W2')
+            self.b2 = tf.Variable(tf.zeros(shape=[10]), name='b2')
+            self.output = tf.nn.softmax(tf.matmul(self.y1, self.W2) + self.b2)
+            #net = tl.layers.InputLayer(self.input,"In")
+            #net = tl.layers.DenseLayer(net,n_units=net_size,act = tf.nn.sigmoid,W_init = w_init, name="sigmoid1")
+            #net = tl.layers.DenseLayer(net,n_units=10,act =tf.nn.softmax,W_init = w_init, name="softmax1")
             self.label = tf.placeholder(tf.float32,[None,10])
             loss = tf.reduce_mean(tf.square(self.output-self.label))
             self.loss = loss
-            self.params = net.all_params
-            self.sigmoid_params = tl.layers.get_variables_with_name("optimizee%d/sigmoid1"%times)
-            self.softmax_params = tl.layers.get_variables_with_name("optimizee%d/softmax1"%times)
+            self.params = [self.W1,self.b1,self.W2,self.b2]
+            self.sigmoid_params =[self.W1,self.b1] #tl.layers.get_variables_with_name("optimizee%d/sigmoid1"%times)
+            self.softmax_params =[self.W2,self.b2] #tl.layers.get_variables_with_name("optimizee%d/softmax1"%times)
             self.gradients = tf.gradients(loss,self.params)
             self.sigmoid_gradients = tf.gradients(loss,self.sigmoid_params)
             self.softmax_gradients = tf.gradients(loss, self.softmax_params)
@@ -123,34 +129,54 @@ class train:
         self.update_softmax = tf.reshape(self.update_softmax,[self.update_softmax.shape[0]])
         self.apply_grad_op = []
         num = 0
+        i = 0
         for param in self.sigmoid_params:
+
             if len(param.shape) == 1:
-                param = param + self.update_sigmoid[num:num + int(param.shape[0])]
+                params = tf.add(param , self.update_sigmoid[num:num + int(param.shape[0])])
+                self.sigmoid_params[i] = params
                 num = num + int(param.shape[0])
+                i = i + 1
 
             elif len(param.shape) == 2:
-                param= param + tf.reshape(self.update_sigmoid[num: num + int(param.shape[0]*int(param.shape[1]))],[param.shape[0],param.shape[1]])
+                params = tf.add(param , tf.reshape(self.update_sigmoid
+                                          [num: num + int(param.shape[0]*int(param.shape[1]))],
+                                          [param.shape[0],param.shape[1]]))
+                self.sigmoid_params[i] = params
                 num = num + int(param.shape[0])*int(param.shape[1])
+                i = i + 1
         num = 0
+        i = 0
         for param in self.softmax_params:
             if len(param.shape) == 1:
-                param=param + self.update_softmax[num:num + int(param.shape[0])]
+                params=tf.add(param , self.update_softmax[num:num + int(param.shape[0])])
+                self.softmax_params[i] = params
                 num = num + int(param.shape[0])
-
+                i = i + 1
             elif len(param.shape) == 2:
-                param = param + tf.reshape(
+                params = tf.add(param , tf.reshape(
                         self.update_softmax[num: num + int(param.shape[0]) * int(param.shape[1])],
-                        [param.shape[0], param.shape[1]])
+                        [param.shape[0], param.shape[1]]))
+                self.softmax_params[i] = params
+                i = i + 1
                 num = num + int(param.shape[0]) * int(param.shape[1])
-        self.loss = tf.reduce_mean(tf.square(self.output-self.label))
 
+
+        self.y1_new = tf.nn.sigmoid(tf.matmul(self.input, self.sigmoid_params[0]) + self.sigmoid_params[1])
+        self.output_new = tf.nn.sigmoid(tf.matmul(self.y1_new, self.softmax_params[0]) + self.softmax_params[1])
+        self.loss_new = tf.reduce_mean(tf.square(self.output_new-self.label))
+        self.assign_op = []
+        self.assign_op.append(self.W1.assign(self.sigmoid_params[0]))
+        self.assign_op.append(self.b1.assign(self.sigmoid_params[1]))
+        self.assign_op.append(self.W2.assign(self.softmax_params[0]))
+        self.assign_op.append(self.b2.assign(self.softmax_params[1]))
         #self.update_sigmoid = None
         #self.update_softmax = None
 
     def update(self):
 
-        self.sigmoid_optimizer.train(self.loss)
-        self.softmax_optimizer.train(self.loss)
+        self.sigmoid_optimizer.train(self.loss_new)
+        self.softmax_optimizer.train(self.loss_new)
 
 
     def save_opti(self):
@@ -166,21 +192,23 @@ class train:
             W = mini_batch[0]
             y = mini_batch[1]
             feed_dict = {self.input: W, self.label: y}
-            loss,sigmoid_params,softmax_gradients,update_sigmoid= self.sess.run([self.loss,self.sigmoid_params,self.softmax_gradients,self.sigmoid_optimizer.output],feed_dict = feed_dict)
-            print(sigmoid_params[0][0][65])
+            loss= self.sess.run([self.loss],feed_dict = feed_dict)
+            #print(sigmoid_params[0][0][65])
 
-            print(update_sigmoid.nonzero())
+            #print(update_sigmoid.nonzero())
 
             #print(update_softmax)
             #self.out_grads(sigmoid_gradients,softmax_gradients)
             #self.sess.run([self.apply_grad_op],feed_dict = feed_dict)
             #self.apply_grads()
             #self.update()
-            #self.sess.run([self.sigmoid_optimizer.train_op],feed_dict = feed_dict)
-            #self.sess.run([self.softmax_optimizer.train_op], feed_dict=feed_dict)
+            self.sess.run([self.sigmoid_optimizer.train_op],feed_dict = feed_dict)
+            self.sess.run([self.softmax_optimizer.train_op], feed_dict=feed_dict)
+            self.sess.run([self.apply_grad_op])
             if i %10 ==0:
                 print(loss)
                 losses.append(loss)
+
 
 
         writer = tf.summary.FileWriter("D://sc_a3c//logs", self.sess.graph)
@@ -215,7 +243,7 @@ trainer = train(sess)
 #trainer.train_contrast()
 trainer.train_one_fun()
 #trainer.save_ckpt()
-trainer.save_opti()
+#trainer.save_opti()
 #optimizer_0 = grader(hidden_size,layers,batch_size,0,lr)
 #optimizer_0.feed(tf.reshape(params[0][0][0],[1,1,1]))
 
